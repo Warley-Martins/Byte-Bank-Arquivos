eafb69374dc61ccffe95e7ddbb704e4b0b079c96
Warley Junio Martins Vieira <warley.vieira@qwti.com.br>
2020-08-18 20:54:49 -0300

    Manipulação de arquivos

---
+++ src/ByteBank-Arquivos/ByteBank-Arquivos/Cliente2.cs
@@ -0,0 +1,21 @@
+﻿using _2_ByteBank;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ByteBank_Arquivos
+{
+    class Cliente2 : Cliente
+    {
+        public Cliente2(string nome, string cpf, string telefone)
+            : base(nome, cpf, telefone)
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"{Nome},{CPF},{Telefone},{contaCorrente.Agencia},{contaCorrente.Conta},{contaCorrente.Senha}";
+        }
+    }
+}
--- src/ByteBank-Arquivos/ByteBank-Arquivos/Program.cs
+++ src/ByteBank-Arquivos/ByteBank-Arquivos/Program.cs
@@ -1,5 +1,6 @@
 ﻿using _2_ByteBank;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -9,9 +10,116 @@ namespace ByteBank_Arquivos
     {
         static void Main(string[] args)
         {
+
+        }
+        public static void RecuperarDados()
+        {
+            Console.Write("Digite o nome do arquivo que deseja Recuperar: ");
+            var arquivo = Console.ReadLine();
+            if (arquivo.IndexOf('.') == -1)
+            {
+                arquivo += ".txt";
+            }
+            var clientes = new List<Cliente2>();
+            using (var fluxoDeArquivo = new FileStream(arquivo, FileMode.Open))
+            using (StreamReader leitor = new StreamReader(fluxoDeArquivo))
+            {
+                while (!leitor.EndOfStream)
+                {
+                    var dadosCliente = leitor.ReadLine();
+                    var atributos = dadosCliente.Split(',');
+                    var nome = atributos[0];
+                    var cpf = atributos[1];
+                    var telefone = atributos[2];
+                    Cliente2 c = new Cliente2(nome, cpf, telefone);
+                    var numeroAgencia = int.Parse(atributos[3]);
+                    var numeroConta = int.Parse(atributos[4]);
+                    var senha = atributos[5];
+                    ContaCorrente conta = new ContaCorrente(numeroAgencia, numeroConta, senha);
+                    c.contaCorrente = conta;
+                    clientes.Add(c);
+                }
+            }
+        }
+        private static void LeituraArquivo()
+        {
+            EscritaEmArquivo();
+            Console.Write("Digite o nome do arquivo que deseja ler: ");
+            var arquivo = Console.ReadLine();
+            if (arquivo.IndexOf('.') == -1)
+            {
+                arquivo += ".txt";
+            }
+            using (var fluxoDeArquivo = new FileStream(arquivo, FileMode.Open))
+            using (StreamReader leitor = new StreamReader(fluxoDeArquivo))
+            {
+                while (!leitor.EndOfStream)
+                {
+                    Console.WriteLine(leitor.ReadLine());
+                }
+            }
+        }
+
+        private static void EscritaBinaria()
+        {
             Console.Write("Digite o nome do arquivo que deseja criar: ");
             var arquivo = Console.ReadLine();
-            if(arquivo.IndexOf('.') == -1)
+            if (arquivo.IndexOf('.') == -1)
+            {
+                arquivo += ".txt";
+            }
+            using (var fluxoDeArquivo = new FileStream(arquivo, FileMode.Create))
+            using (BinaryWriter escritor = new BinaryWriter(fluxoDeArquivo))
+            {
+                Console.Write("Digite a quantidade de clientes que deseja salvar: ");
+                int quantidadeClientes = int.Parse(Console.ReadLine());
+                for (int i = 0; i < quantidadeClientes; i++)
+                {
+
+                    Cliente2 c = CadastrarNovoCliente();
+                    escritor.Write(c.Nome);
+                    escritor.Write(c.CPF);
+                    escritor.Write(c.Telefone);
+                    escritor.Write(c.contaCorrente.Agencia);
+                    escritor.Write(c.contaCorrente.Conta);
+                }
+            }
+        }
+        private static void LeituraBinaria()
+        {
+            Console.Write("Digite o nome do arquivo que deseja Ler: ");
+            var arquivo = Console.ReadLine();
+            if (arquivo.IndexOf('.') == -1)
+            {
+                arquivo += ".txt";
+            }
+            using (var fluxoDeArquivo = new FileStream(arquivo, FileMode.Open))
+            using (var leitor = new BinaryReader(fluxoDeArquivo))
+            {
+                Console.Write("Digite a quantidade de clientes que deseja salvar: ");
+                int quantidadeClientes = int.Parse(Console.ReadLine());
+
+                var nome = leitor.Read();
+                var telefone = leitor.Read();
+                var cpf = leitor.Read();
+                var numeroAgencia = leitor.ReadInt32();
+                var numeroConta = leitor.ReadInt32();
+                var senha = leitor.Read();
+                var texto = $"{nome}{cpf}{telefone}{numeroAgencia}{numeroConta}{senha}";
+                if (arquivo.IndexOf(".cvs") != -1)
+                {
+                    texto.Replace(',', ' ');
+                }
+                Console.WriteLine(texto);
+
+            }
+        }
+
+        private static void EscritaEmArquivo()
+        {
+            Console.Write("Digite o nome do arquivo que deseja criar: ");
+            var arquivo = Console.ReadLine();
+            if (arquivo.IndexOf('.') == -1)
             {
                 arquivo += ".txt";
             }
@@ -27,7 +135,7 @@ namespace ByteBank_Arquivos
             }
         }
 
-        private static Cliente CadastrarNovoCliente()
+        private static Cliente2 CadastrarNovoCliente()
         {
             Console.Write("Digite o nome do cliente: ");
             var nome = Console.ReadLine();
@@ -35,14 +143,14 @@ namespace ByteBank_Arquivos
             var cpf = Console.ReadLine();
             Console.Write("Digite o telefone do cliente: ");
             var telefone = Console.ReadLine();
-            Cliente c = new Cliente(nome, cpf, telefone);
+            Cliente2 c = new Cliente2(nome, cpf, telefone);
             Console.Write("Digite a senha do cliente: ");
             var senha = Console.ReadLine();
             Console.Write("Digite a agencia da conta do cliente: ");
             var numeroAgencia = int.Parse(Console.ReadLine());
             Console.Write("Digite o numero da conta do cliente: ");
             var numeroConta = int.Parse(Console.ReadLine());
-            ContaCorrente conta = new ContaCorrente(numeroAgencia,numeroConta, senha);
+            ContaCorrente conta = new ContaCorrente(numeroAgencia, numeroConta, senha);
             c.contaCorrente = conta;
             return c;
 
